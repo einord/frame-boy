79009c93d895360803e9f47e996d415d945de82d
Jonatan Nyqvist <jonte@plik.se>
2023-01-13 07:25:08 +0100

    Created a working electron vue 3 application.

---
+++ frame-boy/src/components/application-footer.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="application-footer">
+    <span>Frame Boy</span>
+</div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+.application-footer {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
