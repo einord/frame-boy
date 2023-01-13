eb86e0e954c18ba58f3325ca1fb7f632d1166502
Jonatan Nyqvist <jonte@plik.se>
2023-01-13 07:25:47 +0100

    Created a working electron vue 3 application. (#1)

---
+++ frame-boy/src-electron/electron-preload.ts
@@ -0,0 +1,39 @@
+/**
+ * This file is used specifically for security reasons.
+ * Here you can access Nodejs stuff and inject functionality into
+ * the renderer thread (accessible there through the "window" object)
+ *
+ * WARNING!
+ * If you import anything from node_modules, then make sure that the package is specified
+ * in package.json > dependencies and NOT in devDependencies
+ *
+ * Example (injects window.myAPI.doAThing() into renderer thread):
+ *
+ *   import { contextBridge } from 'electron'
+ *
+ *   contextBridge.exposeInMainWorld('myAPI', {
+ *     doAThing: () => {}
+ *   })
+ *
+ * WARNING!
+ * If accessing Node functionality (like importing @electron/remote) then in your
+ * electron-main.ts you will need to set the following when you instantiate BrowserWindow:
+ *
+ * mainWindow = new BrowserWindow({
+ *   // ...
+ *   webPreferences: {
+ *     // ...
+ *     sandbox: false // <-- to be able to import @electron/remote in preload script
+ *   }
+ * }
+ */
+
+import { contextBridge, FileFilter } from 'electron';
+import { dialog } from '@electron/remote';
+// 'electronApi' will be available on the global window context
+contextBridge.exposeInMainWorld('electronApi', {
+  openFileDialog: async (title: string, folder: any, filters: FileFilter[]) => {
+    // calling showOpenDialog from Electron API: https://www.electronjs.org/docs/latest/api/dialog/
+    const response = await dialog.showOpenDialog({ title, filters, properties: ['openFile', 'multiSelections'], }); return response.filePaths;
+  }
+});
---
+++ frame-boy/src/app.vue
@@ -0,0 +1,13 @@
+<template>
+    <router-view class="content" />
+    <application-footer />
+</template>
+
+<script setup lang="ts">
+import ApplicationFooter from './components/application-footer.vue';
+import { useQuasar } from 'quasar';
+
+const $q = useQuasar();
+$q.dark.set(true); // TODO: Make this dynamic in the applications settings
+
+</script>
