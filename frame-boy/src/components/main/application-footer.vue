4d9696e68960b34b150fae9c20ea97f95e87aa12
Jonatan Nyqvist <jonte@plik.se>
2023-01-22 10:20:33 +0100

    Worked with css and footer.

--- frame-boy/src/components/main/application-footer.vue
+++ frame-boy/src/components/main/application-footer.vue
@@ -1,17 +1,23 @@
 <template>
 <div class="application-footer">
     <div class="space left-space">
-        <fa icon="gear"></fa>
+        <router-link to="/settings">
+            <normal-button icon="gear" transparent />
+        </router-link>
     </div>
     <nav>
-        <router-link to="/settings">Settings</router-link>
-        <router-link to="/">Home</router-link>
+        <router-link to="/">
+            <normal-button class="default" icon="home" transparent />
+        </router-link>
     </nav>
-    <div class="space right-space">Frame Boy @ {{ new Date().getFullYear() }}</div>
+    <div class="space right-space">
+        <!-- Frame Boy &copy; {{ new Date().getFullYear() }} -->
+    </div>
 </div>
 </template>
 
 <script setup lang="ts">
+import NormalButton from "../common/normal-button.vue";
 
 </script>
 
@@ -22,8 +28,8 @@
     align-items: center;
     width: auto;
     gap: 1rem;
-    margin: 0 0.5rem;
-    font-size: $font-size-small;
+    margin: 0 $spacing-small;
+    font-size: $font-size-normal;
 
     > .left-space {
         text-align: left;
@@ -35,21 +41,25 @@
         gap: 0.5em;
 
         > a {
-            color: $text-color;
-            text-decoration: none;
-            transition: color 0.2s ease-in-out;
-            border-radius: $border-radius;
-            padding: 0.25em 0.5em;
+        //     color: $text-color;
+        //     text-decoration: none;
+        //     transition: color 0.2s ease-in-out;
+        //     border-radius: $border-radius;
+        //     padding: 0.25em 0.5em;
 
-            &:hover {
-                color: $text-color-hover;
-                background-color: $hover-background;
+        //     &:hover {
+        //         color: $text-color-hover;
+        //         background-color: $hover-background;
+        //     }
+            > .default {
+                font-size: $font-size-large;
             }
         }
     }
 
     > .right-space {
         text-align: right;
+        font-size: $font-size-small;
     }
 }
 </style>
