aa692f83acf985dd8a89ee57e42d40e091fe40b5
Jonatan Nyqvist <jonte@plik.se>
2023-01-27 21:42:22 +0100

    Added more buttons to the settings view.

--- frame-boy/src/views/settings.vue
+++ frame-boy/src/views/settings.vue
@@ -3,13 +3,19 @@
     <h1>Settings</h1>
     <div class="items">
         <large-button icon="gear" title="General" to="/settings/general">
-            General settings of the application
+            General settings of the Frame Boy
         </large-button>
         <large-button icon="table-columns" title="Dashboard">
             Dashboard widgets, their locations and sizes
         </large-button>
         <large-button icon="shield-halved" title="Security">
-            Select the security level of the application
+            Select the security level of the Frame Boy such as locking the settings with a key code
+        </large-button>
+        <large-button icon="bolt" title="Energy">
+            Tweak the power consumption of the Frame Boy to prolong its life and your electricity bill
+        </large-button>
+        <large-button icon="book-medical" title="Logs">
+            Read all the fancy and useful logs from the Frame Boy and its components
         </large-button>
     </div>
 </div>
@@ -25,10 +31,8 @@ import LargeButton from '../components/common/large-button.vue';
     text-align: center;
 
     > .items {
-        // display: flex;
-        // justify-content: center;
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(20rem, auto));
+        grid-template-columns: repeat(auto-fit, 20rem);
         justify-content: center;
         
         margin: $spacing-large;
